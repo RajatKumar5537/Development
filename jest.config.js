696c2ca9a158bbc05079f6b0033c26685fa0075c
Rajat <kumarrajatpadhan5537@gmail.com>
2025-09-17 12:46:44 +0530

    modify config file

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,13 @@
-// /** @type {import('jest').Config} */
-// const config = {
-//   testEnvironment: "jsdom",
-//   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-//   moduleNameMapper: {
-//     "^@/(.*)$": "<rootDir>/$1",
-//   },
-// };
-
-// export default config;
-
 module.exports = {
   testEnvironment: 'jsdom',
   testMatch: ['**/tests/**/*.test.js', '**/tests/**/*.spec.js'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   transform: {
-    '^.+\\.(js|jsx)$': 'babel-jest'
+    '^.+\\.(js|jsx)$': ['babel-jest', {
+      presets: [
+        ['@babel/preset-env'],
+        ['@babel/preset-react', { runtime: 'automatic' }]
+      ]
+    }]
   }
 }
